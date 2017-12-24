6299ea91d33cfae91b982b6d0859d52676f9fc57
Ruanovsky <grivermaster@gmail.com>
2017-12-24 15:46:22 +0300

    Окно авторизации

--- Abiturients_App/Abiturients_App/Form1.cs
+++ Abiturients_App/Abiturients_App/Form1.cs
@@ -10,12 +10,21 @@ using System.Windows.Forms;
 
 namespace Abiturients_App
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
-            //sdfs
+            this.MaximumSize = new System.Drawing.Size(300, 400);
+            MaterialSkin.MaterialSkinManager skinManager = MaterialSkin.MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.DARK;
+            skinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Green600, MaterialSkin.Primary.Green900, MaterialSkin.Primary.BlueGrey500, MaterialSkin.Accent.Orange700, MaterialSkin.TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
